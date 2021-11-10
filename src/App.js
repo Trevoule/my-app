5e25be07f507260b4d3ce97391ce4b2ea1a19758
trevoule <wladyslaw.reginiewicz@gmail.com>
2021-11-10 20:08:17 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import './styles/App.css'
+import MyButton from './UI/MyButton';
+
+// import ClassCounter from './components/ClassCounter';
+// import Counter from './components/Counter';
+import PostList from './components/PostList';
+import MyInput from './UI/MyInput';
 
 function App() {
+
+  const [posts] = useState([
+        { id: 1, title: 'JavaScript', body: 'Description' },
+        { id: 2, title: 'JavaScript', body: 'Description' },
+        { id: 3, title: 'JavaScript', body: 'Description' }
+  ]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <MyInput type="text" placeholder="Название поста" />
+        <MyInput type="text" placeholder="Описание поста" />
+        <MyButton disabled>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title='Список постов про JS' />
     </div>
   );
 }
---
+++ src/UI/MyButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from './myButton.module.css';
+
+
+
+const MyButton = ({children, ...props}) => {
+    return (
+        <button {...props} className={classes.myButton}>
+            {children}
+        </button>
+    )
+}
+
+export default MyButton
